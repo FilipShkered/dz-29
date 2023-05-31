2f713f2e27e9c3cfb32aa2cdf0461aac90cea720
Filip <example@gmail.com>
2023-05-31 18:34:14 +0300

    first commit

---
+++ lifting/src/App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+function Name({ name, onNameChange }) {
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input id="name" value={name} onChange={onNameChange} />
+    </div>
+  );
+}
+
+function FavoriteAnimal({ animal, onAnimalChange }) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input
+        id="animal"
+        value={animal}
+        onChange={(event) => onAnimalChange(event.target.value)}
+      />
+    </div>
+  );
+}
+
+function Display({ name, animal }) {
+  return <div>{`Hey ${name}, your favorite animal is ${animal}!`}</div>;
+}
+
+function App() {
+  const [name, setName] = React.useState('');
+  const [animal, setAnimal] = React.useState('');
+
+  return (
+    <form>
+      <Name name={name} onNameChange={(event) => setName(event.target.value)} />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={(value) => setAnimal(value)}
+      />
+      <Display name={name} animal={animal} />
+    </form>
+  );
+}
+
+export default App;
